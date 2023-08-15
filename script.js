5f5519181c693c5071e7afdb383895f57679a3b8
Ashishkuldeep23 <ashishkuldeep6@gmail.com>
2023-08-15 18:58:43 +0530

    National Anthum added fixed err

--- script.js
+++ script.js
@@ -3,27 +3,29 @@
 // // // Arrow animation here ------->
 let arrows = document.querySelectorAll(".arrow")
 
-console.log(arrows)
+// console.log(arrows)
 
-arrows.forEach( (arrow , i)=>{
-    arrow.style.transform = `rotate(${(i)*15}deg)` 
-    arrow.style.animation  = `arrow 1 ease-in-out ${i*0.15}s ` 
+arrows.forEach((arrow, i) => {
+    arrow.style.transform = `rotate(${(i) * 15}deg)`
+    arrow.style.animation = `arrow 1 ease-in-out ${i * 0.15}s `
 })
 
 
 
 
+// // // Play music by js ------->
+let audio = new Audio("./play.mp3")  // // // Prepare audio for paly
+
 
 // // // Mouse move on main
+document.querySelector("main").addEventListener("mousemove", (dets) => {
+    // console.log(dets)
 
-document.querySelector("main").addEventListener("mousemove" , (dets)=>{
-    console.log(dets)
+    audio.play()   // // If mouse move then play music
 
-    document.getElementById("tracker").style.transform = `translate(${dets.clientX-10}px , ${dets.clientY-10}px)`
-} )
+    document.getElementById("tracker").style.transform = `translate(${dets.clientX - 10}px , ${dets.clientY - 10}px)`
+})
 
 
-// // // Play music by js ------->
-const audio = new Audio("play.mp3")
-audio.play()
+
 
