3d02b5ec1c458cad8e75967f3253917df766e298
Ashishkuldeep23 <ashishkuldeep6@gmail.com>
2023-08-18 19:44:38 +0530

    Sound off permanently

--- script.js
+++ script.js
@@ -13,19 +13,19 @@ arrows.forEach((arrow, i) => {
 
 
 
-// // // Play music by js ------->
-let audio = new Audio("./play.mp3")  // // // Prepare audio for paly
-
 
 // // // Mouse move on main
 document.querySelector("main").addEventListener("mousemove", (dets) => {
     // console.log(dets)
-
-    audio.play()   // // If mouse move then play music
 
     document.getElementById("tracker").style.transform = `translate(${dets.clientX - 10}px , ${dets.clientY - 10}px)`
 })
 
 
 
+// // // // Play music by js ------->
+// let audio = new Audio("./play.mp3")  // // // Prepare audio for paly
+
+
+// audio.play()   // // If mouse move then play music
 
